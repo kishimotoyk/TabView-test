9f26a2622bec332737034963a90dfee9797d9ffe
kishimoto <kissy0613@gmail.com>
2018-06-15 22:25:44 +0900

    コメント追加と要らないものをいろいろ削除

--- app/src/main/java/org/t_robop/tabview/MainActivityFragment.java
+++ app/src/main/java/org/t_robop/tabview/MainActivityFragment.java
@@ -1,17 +1,15 @@
 package org.t_robop.tabview;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+/*****
+ * 特にTextViewしか配置していないので、何もしていないです
+ * もしJavaファイル上でレイアウトをいじりたいのなら、onCreateViewの中に書いてください
+ *****/
 
 public class MainActivityFragment extends Fragment {
 //    @Override
@@ -23,7 +21,7 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main_activity, container, false);
-
+        //ここに書く
         return view;
     }
 
